69aeffbb13d557f00edc73a967396466fd5a388b
MALLIK\Venu Mallik <venu.mallik@insisiv.com>
2021-06-02 21:50:07 +0530

    notinhg

---
+++ src/components/Recording/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useContext , useRef } from "react";
+import { Layout, Row, Col, Divider, Input,Card , Typography } from "antd";
+import { EyeOutlined , LikeOutlined , DislikeOutlined } from "@ant-design/icons";
+import {Context} from '../../context';
+
+
+const BasicRecording=(props)=>{
+    
+
+
+
+
+
+
+}
